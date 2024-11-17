e5349adf7750fc453cf11b1ddce13895b0796808
Peronny <peronnysegundo@gmail.com>
2024-11-17 14:37:54 -0300

    tela de login

--- script.js
+++ script.js
@@ -2,11 +2,13 @@ const form = document.getElementById("formulario");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const senha = document.getElementById("senha")
-const senhaDeConfirmacao = document.getElementById("senhaDeConfirmacao");
+const senhaConfirmacao = document.getElementById("senhaConfirmacao");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    validacaoUsername();
+    if(validacaoDoFormulario){
+        alert("Cadastrado!");
+    }
 })
 
 validacaoUsername = () => {
@@ -16,10 +18,62 @@ validacaoUsername = () => {
     }
     else{
         const item = username.parentElement;
-        item.classList = "conteudoDoFormulario"
+        item.className = "conteudoDoFormulario"
     }
 }
 
+validacaoEmail = () => {
+    const emailValue = email.value;
+    if(emailValue === ""){
+        mostrarErro(email, "O email não pode ser vazio");
+    }
+    else{
+        const item = email.parentElement;
+        item.className = "conteudoDoFormulario"
+    }
+}
+
+validacaoSenha = () => {
+    const senhaValue = senha.value;
+    if(senhaValue === ""){
+        mostrarErro(senha, "A senha não pode ser vazia");
+    }
+    else if(senhaValue.length < 8){
+        mostrarErro(senha, "A senha deve no mínimo 8 caracteres")
+    }
+    else{
+        const item = senha.parentElement;
+        item.className = "conteudoDoFormulario";
+    }
+}
+
+validacaoConfrimarSenha = () => {
+    const senhaValue = senha.value;
+    const senhaConfirmacaoValue = senhaConfirmacao.value;
+    if(senhaConfirmacaoValue === ""){
+        mostrarErro(senhaConfirmacao, "A confirmação de senha não pode ser vazia");
+    }
+    else if(senhaConfirmacaoValue !== senhaValue){
+        mostrarErro(senhaConfirmacao, "Senhas diferentes");
+    }
+    else{
+        const item = senhaConfirmacao.parentElement;
+        item.className = "conteudoDoFormulario";
+    }
+}
+
+validacaoDoFormulario = () => {
+    validacaoUsername();
+    validacaoEmail();
+    validacaoSenha();
+    validacaoConfrimarSenha();  
+    const item = form.querySelectorAll(".conteudoDoFormulario");
+    const valido = [...item].every((item) => {
+        return item.className == "conteudoDoFormulario"
+    });
+    return valido;
+}
+ 
 mostrarErro = (input, menssagem) => {
     const item = input.parentElement;
     const menssagemDeErro = item.querySelector("a");
